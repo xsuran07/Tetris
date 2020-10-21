43493f580e7eb369812bd52fc53d171d29394336
xsuran07 <xsuran07@stud.fit.vutbr.cz>
2020-10-21 22:04:16 +0200

    Perform small refactorization + add comments

--- objects.py
+++ objects.py
@@ -1,6 +1,10 @@
+## @brief Implementation of game objects.
+## @author Jakub Šuráň (xsuran07)
+
 import pygame
 import constants as const
 
+## @class Representation of game object ("square")
 class Shape1:
 	def __init__(self, x, y):
 		self.x = x
@@ -32,22 +36,31 @@ class Shape1:
 				if(square):
 					drawer(self.x - 1 + j, self.y - 2 + i, const.WHITE, self.color)
 
+	## @brief Simple method for getting x-coordinate.
+	## @return Returns object's x-coordinate.
 	def get_x(self):
 		return self.x
 
+	## @brief Simple method for getting y-coordinate.
+	## @return Returns object's y-coordinate.
 	def get_y(self):
 		return self.y
 
+	## @brief Simple method for setting both coordinates.
 	def set_cor(self, new_x, new_y):
 		self.x = new_x
 		self.y = new_y
 
+	## @brief Adds given value to x-coordinate.
 	def add_x(self, value):
 		self.x += value
 
+	## @brief Adds given value to y-coordinate.
 	def add_y(self, value):
 		self.y += value
 	
+	## @brief Finds out which blocks on playing field are occupied by the object.
+	## @return Returns hash map with occupied blocks.
 	def get_active_blocks(self):
 		ret = {}
 
@@ -59,6 +72,8 @@ class Shape1:
 
 		return ret
 	
+	## @brief Adds blocks which object currently occupies into given hash map + checks if object isn't over the top.
+	## @return True if everything is ok, False if any part of object is over the top of playing field.
 	def add_active_blocks(self, struct):
 		ret = True
 
@@ -74,6 +89,7 @@ class Shape1:
 					
 		return ret
 	
+## @class Representation of game object ("long line")
 class Shape2(Shape1):
 	def __init__(self, x, y):
 		super().__init__(x, y)
@@ -97,6 +113,7 @@ class Shape2(Shape1):
 					],
 		]
 
+## @class Representation of game object ("reverse L")
 class Shape3(Shape1):
 	def __init__(self, x, y):
 		super().__init__(x, y)
@@ -120,6 +137,7 @@ class Shape3(Shape1):
 					],
 		]
 
+## @class Representation of game object ("L")
 class Shape4(Shape1):
 	def __init__(self, x, y):
 		super().__init__(x, y)
@@ -143,6 +161,7 @@ class Shape4(Shape1):
 					],
 		]
 
+## @class Representation of game object ("reverse Z")
 class Shape5(Shape1):
 	def __init__(self, x, y):
 		super().__init__(x, y)
@@ -166,6 +185,7 @@ class Shape5(Shape1):
 					], 
 		]
 
+## @class Representation of game object ("Z")
 class Shape6(Shape1):
 	def __init__(self, x, y):
 		super().__init__(x, y)
@@ -189,6 +209,7 @@ class Shape6(Shape1):
 					],
 		]
 
+## @class Representation of game object ("ship")
 class Shape7(Shape1):
 	def __init__(self, x, y):
 		super().__init__(x, y)
