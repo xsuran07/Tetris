d28f23eb73d1dd8b744520025e0161658e7f0df4
xsuran07 <xsuran07@stud.fit.vutbr.cz>
2022-05-07 22:32:00 +0200

    Correct formatting in picker.py

--- picker.py
+++ picker.py
@@ -1,18 +1,36 @@
-## @brief Implementation of class for random picking.
-## @author Jakub Šuráň
+'''
+Author(s): Jakub Šuráň
+
+Implementation of class for random picking.
+'''
 
 import random
 import objects as ob
 
-## @brief Simple class for random picking.
-class Picker:
-	def __init__(self):
-		self.values = [ob.Shape1, ob.Shape2, ob.Shape3, ob.Shape4, ob.Shape5, ob.Shape6, ob.Shape7] 
 
-	## @brief Randomly picks one of the game objects.
-	## @param x X-coordinate of new object.
-	## @param y Y-coordinate of new object.
-	## @return Returns randomly picked game object.
-	def pick(self, x, y):
-		result = random.choice(self.values)
-		return result(x, y)
+class Picker:
+    '''
+    Simple class for random picking.
+    '''
+
+    def __init__(self):
+        self.values = [ob.Shape1, ob.Shape2, ob.Shape3, ob.Shape4, ob.Shape5, ob.Shape6, ob.Shape7]
+
+    def pick(self, x, y):
+        '''
+        Randomly picks one of the game objects.
+
+        Parameters
+        ----------
+        x : int
+            X-coordinate of new object.
+        y : int
+            Y-coordinate of new object.
+        Returns
+        -------
+        ob.Shape1
+            Returns randomly picked game object.
+        '''
+
+        result = random.choice(self.values)
+        return result(x, y)
