3000f89cb80f608e925ed8e8387110a473038f0f
xsuran07 <xsuran07@stud.fit.vutbr.cz>
2022-05-16 20:55:19 +0200

    Introduce new main executable script
    Installation of the package with source has been added into Pipfile.

---
+++ bin/tetris.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+Author(s): Jakub Šuráň
+
+Main executable script of the whole game.
+'''
+
+import pygame
+
+from tetris_sources.game import Game
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    game = Game()
+    game.loop()
+
+    pygame.quit()
